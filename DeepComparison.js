382dd8e83ff1ca17fac2d403e73bef140941f452
Michael Loschiavo <losch103@gmail.com>
2016-03-19 11:25:09 -0700

    Attempt using conditional loops within loops. Failing in case of a nested object 3.19.2016

--- DeepComparison.js
+++ DeepComparison.js
@@ -6,21 +6,57 @@
 
 
 function deepEqual(object1, object2){
-  if (object1 === object2) { //base case;
+  if (object1 === object2) { //base case
     return true;
   }
-  for (var variable in object1) {
-    if (typeof object1[variable] === 'object' && typeof object2[variable] === 'object') {
-      for(var key in object1[variable]){
 
-        if (object1[variable][key]) {
+  //check if properties match
+  var propNames1 = Object.getOwnPropertyNames(object1);
+  var propNames2 = Object.getOwnPropertyNames(object2);
 
+  for (var i = 0; i < propNames1.length; i++) {
+    console.log('loop1');
+    if (propNames1[i] !== propNames2[i]) {
+      return false;
+    }
+  }
+
+  //check if values match
+  for(var key in object1){
+    console.log('loop2');
+
+    //if nested object...
+    if (typeof object1[key] === 'object' && typeof object2[key] === 'object' && object1[key] !== null) {
+      var innerObj1 = object1[key];
+      var innerObj2 = object2[key];
+      var innerPropNames1 = Object.getOwnPropertyNames(innerObj1);
+      var innerPropNames2 = Object.getOwnPropertyNames(innerObj2);
+        //check property names
+        for (var j = 0; j < innerPropNames1.length; j++) {
+          console.log('loop3');
+          if (innerPropNames1[j] !== innerPropNames2[j]) {
+            return false;
+          }
+        }
+        //check property values
+        for(var index in innerObj1){
+          console.log('loop4');
+          if (innerObj1[index] !== innerObj2[index]) {
+            return false;
+          }
         }
       }
-    } //
 
-  }//loop
-return 'x';
+    //in the case of non-nested object
+    if (object1[key] !== object2[key]) {
+      if (typeof object1[key] === 'object' && object2[key] === 'object') {
+        continue;
+      }
+      return false;
+    }
+  }
+    //If everything checks out, return true
+  return true;
 }
 
 
